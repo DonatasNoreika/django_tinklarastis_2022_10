baa85ca7652049af6620f21cdb324294dfc57412
Donatas Noreika <donoras@gmail.com>
2022-10-21 09:34:05 +0300

    Sutvarkytas admin puslapis

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Irasas, Komentaras
+
 
 # Register your models here.
+
+class KomentarasInLine(admin.TabularInline):
+    model = Komentaras
+    extra = 0
+
+
+class IrasasAdmin(admin.ModelAdmin):
+    list_display = ('autorius', 'pavadinimas', 'data')
+    inlines = [KomentarasInLine]
+
+
+class KomentarasAdmin(admin.ModelAdmin):
+    list_display = ('irasas', 'autorius', 'data')
+    list_editable = ('autorius',)
+
+
+admin.site.register(Irasas, IrasasAdmin)
+admin.site.register(Komentaras, KomentarasAdmin)
--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -9,9 +9,22 @@ class Irasas(models.Model):
     tekstas = models.TextField("Tekstas", max_length=5000)
     data = models.DateTimeField("Data", auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Irašas'
+        verbose_name_plural = 'Irašai'
+
+    def __str__(self):
+        return f"{self.autorius} - {self.pavadinimas} ({self.data})"
 
 class Komentaras(models.Model):
     irasas = models.ForeignKey('Irasas', on_delete=models.CASCADE, related_name="komentarai")
     autorius = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     tekstas = models.TextField("Tekstas", max_length=5000)
     data = models.DateTimeField("Data", auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Komentaras'
+        verbose_name_plural = 'Komentarai'
+
+    def __str__(self):
+        return f"{self.autorius} ({self.data})"
