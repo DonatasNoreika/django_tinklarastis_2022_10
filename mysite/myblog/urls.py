c2c3b165904d3042d70ebea27b171fe13342b4c8
Donatas Noreika <donoras@gmail.com>
2022-10-21 09:56:02 +0300

    Pridėtas pirminis įrašų atvaizdavimo puslapis

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -1,5 +1,6 @@
-
+from django.urls import path
+from .views import IrasasListView
 
 urlpatterns = [
-
+    path('irasai', IrasasListView.as_view(), name='irasai'),
 ]
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Irasas, Komentaras
 
 # Create your views here.
+
+class IrasasListView(generic.ListView):
+    model = Irasas
+    template_name = "irasai.html"
+    context_object_name = "irasai"
