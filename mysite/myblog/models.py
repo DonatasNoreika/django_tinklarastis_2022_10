084d2de424766ec421205d42fe34e3a0b583789b
Donatas Noreika <donoras@gmail.com>
2022-10-21 09:37:53 +0300

    Įrašo tekstas pakeistas į HTML lauką (TinyMCE)

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Irasas(models.Model):
     autorius = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     pavadinimas = models.CharField("Pavadinimas", max_length=200)
-    tekstas = models.TextField("Tekstas", max_length=5000)
+    tekstas = HTMLField("Tekstas")
     data = models.DateTimeField("Data", auto_now_add=True)
 
     class Meta:
