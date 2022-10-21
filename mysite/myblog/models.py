38cbfd7b1d5f875786806a2dcab302c2b9b014a7
Donatas Noreika <donoras@gmail.com>
2022-10-21 08:58:59 +0300

    Aprašyti Irasas ir Komentaras modeliai

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Irasas(models.Model):
+    autorius = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    pavadinimas = models.CharField("Pavadinimas", max_length=200)
+    tekstas = models.TextField("Tekstas", max_length=5000)
+    data = models.DateTimeField("Data", auto_now_add=True)
+
+
+class Komentaras(models.Model):
+    irasas = models.ForeignKey('Irasas', on_delete=models.CASCADE, related_name="komentarai")
+    autorius = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    tekstas = models.TextField("Tekstas", max_length=5000)
+    data = models.DateTimeField("Data", auto_now_add=True)
